be55e5fa100ec95fc75bceb33c83865e10419665
CedDesu <cedriqueorbista@gmail.com>
2025-03-14 01:48:28 +0800

    copied sum_10numbers code for modification

---
+++ second_batch_practice/difference_10numbers_printer.py
@@ -0,0 +1,13 @@
+"""
+
+Input ten numbers
+numbers = [float(input(f"Enter number: {i+1} ")) for i in range(10)]
+
+raise one number to the other:
+print(sum(numbers))
+
+"""
+
+numbers = [float(input(f"Enter num_{i+1}: ")) for i in range(10)]
+
+print("The sum of all the numbers is", sum(numbers))
