1911952d05bb8ecc553095b44f90372bbbf9b501
CedDesu <cedriqueorbista@gmail.com>
2025-03-14 02:08:01 +0800

    copied odd number counter to modify into even counter

---
+++ second_batch_practice/even_10numbers_counter.py
@@ -0,0 +1,10 @@
+"""
+ask user for 10 inputs and count even numbers
+
+print odd number total
+
+"""
+
+odd_number_input = sum(1 for _ in range(10) if int(input("Enter a number: ")) % 2 == 0)
+
+print("The number of odd numbers is: ", odd_number_input)
