941529d142f99b129458b24e360f0989d680634d
CedDesu <cedriqueorbista@gmail.com>
2025-03-14 01:20:55 +0800

    added copied code and modified to find smaller number

---
+++ second_batch_practice/smaller_2numbers_printer.py
@@ -0,0 +1,17 @@
+"""
+Input two numbers
+
+Check number for smaller number:
+if num_1 < num_2:
+    print()
+
+"""
+
+num_1 = float(input("Enter the first number: "))
+num_2 = float(input("Enter the second number: "))
+
+if num_1 > num_2:
+    print(num_2, "is the smaller number")
+
+elif num_1 < num_2:
+    print(num_1, "is the smaller number")
