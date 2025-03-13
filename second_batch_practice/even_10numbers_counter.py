b2821581559282fcbc956e3bee271a85113f98a4
CedDesu <cedriqueorbista@gmail.com>
2025-03-14 02:08:57 +0800

    finalized into 10 number even counter

--- second_batch_practice/even_10numbers_counter.py
+++ second_batch_practice/even_10numbers_counter.py
@@ -5,6 +5,6 @@ print odd number total
 
 """
 
-odd_number_input = sum(1 for _ in range(10) if int(input("Enter a number: ")) % 2 == 0)
+even_number_input = sum(1 for _ in range(10) if int(input("Enter a number: ")) % 2 == 0)
 
-print("The number of odd numbers is: ", odd_number_input)
+print("The number of even numbers is: ", even_number_input)
