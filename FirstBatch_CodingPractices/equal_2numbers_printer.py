4ff8712f35fb042956a6bb6c02d13caa17cfa881
CedDesu <cedriqueorbista@gmail.com>
2025-03-13 22:13:12 +0800

    Changed purpose of code from NumberEquality checker to "inequality"

--- FirstBatch_CodingPractices/equal_2numbers_printer.py
+++ FirstBatch_CodingPractices/equal_2numbers_printer.py
@@ -1,8 +1,8 @@
 """
 Input two numbers
 
-Check number for inequality:
-if num_1 != num_2:
+Check number for equality:
+if num_1 == num_2:
     print()
 
 """
@@ -10,5 +10,5 @@ if num_1 != num_2:
 num_1 = float(input("Enter the first number: "))
 num_2 = float(input("Enter the second number: "))
 
-if num_1 != num_2:
-    print("Not Equal")
+if num_1 == num_2:
+    print("Equal")
