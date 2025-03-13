b5def31ad9e86e0539b970d00fe68f37cd8ed3c2
CedDesu <cedriqueorbista@gmail.com>
2025-03-14 01:46:49 +0800

    finalized code for remainder printer

--- second_batch_practice/remainder_printer.py
+++ second_batch_practice/remainder_printer.py
@@ -3,8 +3,12 @@
 Input two numbers
 num = int(input("Enter number: "))
 
-
 get the remainder of two numbers using modulus operator (%)
 num_1 % num_2
 
 """
+
+num_1 = float(input("Enter the first number: "))
+num_2 = float(input("Enter the second number: "))
+
+print (num_1 % num_2)
