e4741633e47bac10f0d11d9125cdf07760e5f1d7
CedDesu <cedriqueorbista@gmail.com>
2025-03-14 02:44:28 +0800

    wrote final code and confirmed working status.

--- second_batch_practice/all_numbers_between_2.py
+++ second_batch_practice/all_numbers_between_2.py
@@ -6,5 +6,9 @@ num = float(input("Enter number: "))
 print all numbers between two numbers using for loop and range
 """
 
+num_1 = int(input("Enter the first number: "))
+num_2 = int(input("Enter the second number: "))
 
+for i in range (num_1 + 1, num_2 ):
+    print (i)
 
