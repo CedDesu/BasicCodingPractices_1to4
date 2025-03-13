b4f784b5d18ece21195116a3bf8f63b9142effc9
CedDesu <cedriqueorbista@gmail.com>
2025-03-13 22:07:51 +0800

    Added deleted pseudocode for better understanding

---
+++ FirstBatch_CodingPractices/notequal_2numbers_printer.py
@@ -0,0 +1,14 @@
+"""
+Input two numbers
+
+Check number for inequality:
+if num_1 != num_2:
+    print()
+
+"""
+
+num_1 = float(input("Enter the first number: "))
+num_2 = float(input("Enter the second number: "))
+
+if num_1 != num_2:
+    print("Not Equal")
--- FirstBatch_CodingPractices/smaller_2numbers_printer.py
+++ FirstBatch_CodingPractices/smaller_2numbers_printer.py
@@ -1,3 +1,12 @@
+"""
+Input two numbers
+
+Check number for biggest number:
+if num_1 > num_2:
+    print()
+
+"""
+
 num_1 = float(input("Enter the first number: "))
 num_2 = float(input("Enter the second number: "))
 
