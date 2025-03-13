9043406ccbb25d2f5cd78702307764a8934358f6
CedDesu <cedriqueorbista@gmail.com>
2025-03-14 00:26:34 +0800

    added print for odd_number_input and finalized code

--- FirstBatch_CodingPractices/odd_10numbers_printer.py
+++ FirstBatch_CodingPractices/odd_10numbers_printer.py
@@ -5,5 +5,6 @@ print odd number total
 
 """
 
-odd_number_input = sum(1 for _ in range(10) if int((input("Enter a number: ": )) % 2 != 0)
+odd_number_input = sum(1 for _ in range(10) if int(input("Enter a number: ")) % 2 != 0)
 
+print("The number of odd numbers is: ", odd_number_input)
