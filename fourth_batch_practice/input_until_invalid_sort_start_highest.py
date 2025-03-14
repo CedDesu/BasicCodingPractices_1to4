3b730d1c1ddaa5ff86d712fb7b309c942fce1f21
CedDesu <cedriqueorbista@gmail.com>
2025-03-14 15:24:52 +0800

    entered pseudocode and reference code from previous batch for modification into sort from lowest to highest into highest to lowest

---
+++ fourth_batch_practice/input_until_invalid_sort_start_highest.py
@@ -0,0 +1,34 @@
+"""
+initialize numbers
+
+use while loop to ask until invalid user input
+
+while True:
+    number_input = input("Enter a number (or any non-number to stop): ")
+
+    if not number_input.isdigit():
+        print("Invalid input. Exiting program.")
+        break
+
+use numbers.sort() function to sort from lowest to highest
+
+print number_inputs
+"""
+
+numbers = []
+
+while True:
+    number_input = input("Enter a number (or any non-number to stop): ")
+
+    if not number_input.isdigit():
+        print("Invalid input. Exiting program.")
+        break
+
+    numbers.append(int(number_input))
+
+numbers.sort()
+
+if numbers:
+    print(numbers, end=" ")
+else:
+    print("No valid numbers were entered.")
