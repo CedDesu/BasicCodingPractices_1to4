4756bc11ad85b0be72e9709f40fbf06cfeea4d3c
CedDesu <cedriqueorbista@gmail.com>
2025-03-14 15:17:50 +0800

    Added actual code for displaying most duplicates using max, set, and count functions. Added print for testing.

--- fourth_batch_practice/input_until_invalid_display_most_dupes.py
+++ fourth_batch_practice/input_until_invalid_display_most_dupes.py
@@ -1,9 +1,13 @@
 """
+initialize numbers variable for list
+numbers = []
 set up while loop for input until invalid
+
+convert input into integer
+
 """
 
-
-
+numbers = []
 
 while True:
     number_input = input("Enter a number (or any non-number to stop): ")
@@ -11,3 +15,11 @@ while True:
     if not number_input.isdigit():
         print("Invalid input. Exiting program.")
         break
+
+    numbers.append(int(number_input))
+
+if numbers:
+    most_frequent = max(set(numbers), key=numbers.count)
+    frequency = numbers.count(most_frequent)
+
+    print(f"Number with the most duplicates: {most_frequent} (appeared {frequency} times)")
