d2a425cf57e2a2eb83137e334f3df9abe004958a
CedDesu <cedriqueorbista@gmail.com>
2025-03-14 12:46:40 +0800

    applied set and input block. Confirmed working status.

--- third_batch_practice/Input_until_invalid_unique_duplicate.py
+++ third_batch_practice/Input_until_invalid_unique_duplicate.py
@@ -17,5 +17,12 @@ else:
     first_entries.add(number_input) - used to store the inputs into first_entries
 """
 
+first_entries = set()
 
+while True:
+    number_input = input("Enter a number (or any non-number to stop): ")
+
+    if not number_input.isdigit():
+        print("Invalid input. Exiting program.")
+        break
 
