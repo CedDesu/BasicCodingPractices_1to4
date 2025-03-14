6d5d67bb7772f39edabca2c9bec7f636976404d3
CedDesu <cedriqueorbista@gmail.com>
2025-03-14 13:35:04 +0800

    copied previous code for pseudocode reference and added fit modifications

---
+++ third_batch_practice/input_until_invalid_sort.py
@@ -0,0 +1,17 @@
+"""
+initialize lowest
+
+use while loop to ask until invalid user input
+
+while True:
+    number_input = input("Enter a number (or any non-number to stop): ")
+
+    if not number_input.isdigit():
+        print("Invalid input. Exiting program.")
+        break
+
+use numbers.sort() function to sort from lowest to highest
+
+print number_inputss
+"""
+
