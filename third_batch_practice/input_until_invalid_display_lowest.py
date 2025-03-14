0f62701dab2737eebe9ebf53aed16d119467a70e
CedDesu <cedriqueorbista@gmail.com>
2025-03-14 13:17:37 +0800

    added reference pseudocode from previous code and added modifications

---
+++ third_batch_practice/input_until_invalid_display_lowest.py
@@ -0,0 +1,19 @@
+"""
+initialize lowest
+
+use while loop to ask until invalid user input
+
+while True:
+    number_input = (int(input("Enter a number (or any non-number to stop): "))
+
+    if not number_input.isdigit():
+        print("Invalid input. Exiting program.")
+        break
+
+use less than function and if to determine lowest number:
+    if lowest is None or number < lowest:
+        lowest = number_input
+
+use if and not functions to print the lowest number
+"""
+
