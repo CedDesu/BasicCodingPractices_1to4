ccc395a6b9d5a674aa84a4c4e6880e354e271285
CedDesu <cedriqueorbista@gmail.com>
2025-03-14 15:08:32 +0800

    wrote final print code to get average using sum and len functions. confirmed working status.

--- fourth_batch_practice/input_until_valid_number_average.py
+++ fourth_batch_practice/input_until_valid_number_average.py
@@ -10,12 +10,20 @@ while True:
         print("Invalid input. Exiting program.")
         break
 
+use sum() to get sum of all numbers entered and use len() to count number of inputs
 """
-
+numbers = []
 while True:
-    number_input = (int(input("Enter a number (or any non-number to stop): "))
+    number_input = (input("Enter a number (or any non-number to stop): "))
 
     if not number_input.isdigit():
         print("Invalid input. Exiting program.")
         break
 
+    numbers.append(int(number_input))
+
+if numbers:
+    average = sum(numbers) / len(numbers)
+    print(average)
+else:
+    print("No valid numbers were entered.")
