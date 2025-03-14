8f903db1fb4a1531fe238605bd914422b9261ed7
CedDesu <cedriqueorbista@gmail.com>
2025-03-14 12:13:38 +0800

    written pseudocode and possible code to find first entries

---
+++ third_batch_practice/display_first_nonduplicate_number.py
@@ -0,0 +1,13 @@
+"""
+ask user for 10 inputs and display non duplicates
+
+use set to store first entries
+
+for first entries:
+for num in numbers:
+    if num not in first_entry:
+        first_entry.add(num)
+    filtered_numbers.append(num)
+
+print list of numbers
+"""
