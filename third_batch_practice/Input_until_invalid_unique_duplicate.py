8cf672052fa9a446bed4e6bda727fa9aef425fbe
CedDesu <cedriqueorbista@gmail.com>
2025-03-14 12:44:57 +0800

    added full code pseudocode

---
+++ third_batch_practice/Input_until_invalid_unique_duplicate.py
@@ -0,0 +1,21 @@
+"""
+use set to store first_entries
+
+use while loop to ask until invalid user input
+
+while True:
+    number_input = input("Enter a number (or any non-number to stop): ")
+
+    if not number_input.isdigit():
+        print("Invalid input. Exiting program.")
+        break
+
+display duplicate if input is in first_entries
+
+else:
+    print("unique")
+    first_entries.add(number_input) - used to store the inputs into first_entries
+"""
+
+
+
