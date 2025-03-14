f17da07e2e1c00d07e21b60403d0af1b32ad7943
CedDesu <cedriqueorbista@gmail.com>
2025-03-14 14:42:27 +0800

    added copied base code from batch_3 for modification to find highest number

---
+++ fourth_batch_practice/input_until_invalid_diplay_highest.py
@@ -0,0 +1,37 @@
+"""
+initialize lowest
+
+use while loop to ask until invalid user input
+
+while True:
+    number_input = (int(input("Enter a number (or any non-number to stop): "))
+
+    if not number_input.isdigit():
+        print("Invalid input. Exiting program.")
+        break
+
+use less than function and if to determine lowest number:
+    if lowest is None or number < lowest:
+        lowest = number_input
+
+use if and not functions to print the lowest number
+"""
+
+lowest = None
+
+while True:
+    number_input = input("Enter a number (or any non-number to stop): ")
+
+    if not number_input.isdigit():
+        print("Invalid input. Exiting program.")
+        break
+
+    number = int(number_input)
+
+    if lowest is None or number < lowest:
+        lowest = number
+
+if lowest is not None:
+    print("Lowest number entered:", lowest)
+else:
+    print("No valid numbers were entered.")
