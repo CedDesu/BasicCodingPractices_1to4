3188a3bdf2622d75a37653cee97d5cfbbb168467
CedDesu <cedriqueorbista@gmail.com>
2025-03-14 13:40:30 +0800

    added number_input for user and made number_input an integer.

--- third_batch_practice/input_until_invalid_sort.py
+++ third_batch_practice/input_until_invalid_sort.py
@@ -1,5 +1,5 @@
 """
-initialize lowest
+initialize numbers
 
 use while loop to ask until invalid user input
 
@@ -12,6 +12,16 @@ while True:
 
 use numbers.sort() function to sort from lowest to highest
 
-print number_inputss
+print number_inputs
 """
 
+numbers = []
+
+while True:
+    number_input = input("Enter a number (or any non-number to stop): ")
+
+    if not number_input.isdigit():
+        print("Invalid input. Exiting program.")
+        break
+
+    numbers.append(int(number_input))
