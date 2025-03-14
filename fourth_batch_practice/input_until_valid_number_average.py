936d3904e730c1206473df9f8c17ce0a0f1559fb
CedDesu <cedriqueorbista@gmail.com>
2025-03-14 14:56:23 +0800

    added unfinished pseudocode and user_input until invalid code

---
+++ fourth_batch_practice/input_until_valid_number_average.py
@@ -0,0 +1,21 @@
+"""
+initialize lowest
+
+use while loop to ask until invalid user input
+
+while True:
+    number_input = (int(input("Enter a number (or any non-number to stop): "))
+
+    if not number_input.isdigit():
+        print("Invalid input. Exiting program.")
+        break
+
+"""
+
+while True:
+    number_input = (int(input("Enter a number (or any non-number to stop): "))
+
+    if not number_input.isdigit():
+        print("Invalid input. Exiting program.")
+        break
+
